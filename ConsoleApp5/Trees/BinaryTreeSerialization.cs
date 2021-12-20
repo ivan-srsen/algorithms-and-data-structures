cdc4302ecb45ea800fc6e77b3a73bb54cdd12e87
Ivan Srsen <Ivan.Srsen@ysoft.com>
2021-12-20 08:36:32 +0100

    Binary tree serialization and Symmetric binary tree added

---
+++ ConsoleApp5/Trees/BinaryTreeSerialization.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp5.Trees
+{
+    internal class BinaryTreeSerialization
+    {
+        public string Serialize(TreeNode root)
+        {
+            if (root == null)
+                return "X";
+
+            return root.Val.ToString() + "," +  Serialize(root.Left) + "," + Serialize(root.Right);
+        }
+
+        public TreeNode Deserialize(string serialized)
+        {
+            Queue<string> leftQueue = new Queue<string>();
+            var chars = serialized.Split(',');
+            foreach(var c in chars)
+            {
+                leftQueue.Enqueue(c);
+            }
+
+            return DeserializeHelper(leftQueue);
+        }
+
+        private TreeNode DeserializeHelper(Queue<string> leftQueue)
+        {
+            var dequeued = leftQueue.Dequeue();
+
+            if (dequeued == "X")
+                return null;
+
+            var node = new TreeNode(int.Parse(dequeued));
+
+            node.Left = DeserializeHelper(leftQueue);
+            node.Right = DeserializeHelper(leftQueue);
+
+            return node;
+        }
+
+        public class TreeNode
+        {
+            public int Val;
+            public TreeNode Left;
+            public TreeNode Right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.Val = val;
+                this.Left = left;
+                this.Right = right;
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/Trees/SymmetricBinaryTree.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp5.Trees
+{
+    internal class SymmetricBinaryTree
+    {
+        public bool IsBinaryTreeSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+
+            return Check(root.Left, root.Right);
+        }
+
+        private bool Check(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+                return true;
+
+            if(left != null && right != null)
+            {
+                return left.Val == right.Val && Check(left.Right, right.Left) && Check(right.Left, left.Right);
+            }
+
+            return false;
+        }
+
+        public class TreeNode
+        {
+            public int Val;
+            public TreeNode Left;
+            public TreeNode Right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.Val = val;
+                this.Left = left;
+                this.Right = right;
+            }
+        }
+    }
+}
