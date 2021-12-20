cecf9c39e397e48d183c900b5c51d286be47132c
Ivan Srsen <Ivan.Srsen@ysoft.com>
2021-12-20 18:11:29 +0100

    Coin Change

---
+++ ConsoleApp5/DynamicProgramming/CoinChange.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp5.DynamicProgramming
+{
+    internal class CoinChange
+    {
+        public int Change(int[] coins, int sum)
+        {
+            if (sum < 0)
+                return -1;
+
+            if (sum == 0)
+                return 1;
+
+            var bestResult = 0;
+
+            foreach(var coin in coins)
+            {
+                var result = Change(coins, sum - coin);
+
+                if (result < 0)
+                    continue;
+
+                if(result < bestResult)
+                    bestResult = result;
+            }
+
+            if (bestResult > 0)
+                return bestResult + 1;
+
+            return -1;
+        }
+    }
+}
