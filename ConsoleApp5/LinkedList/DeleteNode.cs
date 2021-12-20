e2ff8eb2450f67b1cec2eaa4fa54a27ea12c4ff5
Ivan Srsen <Ivan.Srsen@ysoft.com>
2021-12-20 17:39:59 +0100

    Delete node in the middle of linked list given reference only to that node

---
+++ ConsoleApp5/LinkedList/DeleteNode.cs
@@ -0,0 +1,17 @@
+﻿using LinkedListNode = ConsoleApp5.LinkedList.Nodes.LinkedListNode;
+
+namespace ConsoleApp5.LinkedList
+{
+    internal class DeleteNode
+    {
+        public void Delete(LinkedListNode node)
+        {
+            if (node == null || node.Next == null)
+                return;
+
+            var next = node.Next;
+            node.Value = next.Value;
+            node.Next = next.Next;
+        }
+    }
+}
