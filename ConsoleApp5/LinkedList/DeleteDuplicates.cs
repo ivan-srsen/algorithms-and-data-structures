a698327b7d743f48c0173959faf38022ade89782
Ivan Srsen <Ivan.Srsen@ysoft.com>
2021-12-20 17:30:12 +0100

    Delete duplicates linked list

---
+++ ConsoleApp5/LinkedList/DeleteDuplicates.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using LinkedListNode = ConsoleApp5.LinkedList.Nodes.LinkedListNode;
+
+namespace ConsoleApp5.LinkedList
+{
+    internal class DeleteDuplicates
+    {
+        public void Delete(LinkedListNode root)
+        {
+            var hashTable = new Hashtable();
+
+            var previous = root;
+            var current = root.Next;
+
+            while(current != null)
+            {
+                if (hashTable.ContainsKey(current.Value))
+                {
+                    previous.Next = current.Next;
+
+                    continue;
+                }
+
+                hashTable.Add(current.Value, null);
+
+                previous = current;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/LinkedList/Nodes/LinkedListNode.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp5.LinkedList.Nodes
+{
+    internal class LinkedListNode
+    {
+        public int Value { get; set; }
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(int value)
+        {
+            this.Value = value;
+        }
+
+        public LinkedListNode(int value, LinkedListNode next)
+            : this(value)
+        {
+            this.Next = next;
+        }
+    }
+}
