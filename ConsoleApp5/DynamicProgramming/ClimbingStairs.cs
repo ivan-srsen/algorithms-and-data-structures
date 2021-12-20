f90a6155c5015ec742c5579fea49fe67ee886436
Ivan Srsen <Ivan.Srsen@ysoft.com>
2021-12-20 18:01:23 +0100

    Climbing stairs

---
+++ ConsoleApp5/DynamicProgramming/ClimbingStairs.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp5.DynamicProgramming
+{
+    internal class ClimbingStairs
+    {
+        public int Calculate(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            if (n == 1)
+                return 1;
+
+            if (n == 2)
+                return 2;
+
+            return Calculate(n - 1) + Calculate(n - 2);
+        }
+    }
+}
