ae5d897e4701ee109ccf522f6fae4a782424b63a
Ivan Srsen <Ivan.Srsen@ysoft.com>
2022-01-17 19:37:12 +0100

    Validate binary search tree fix

---
+++ ConsoleApp5/DynamicProgramming/HouseRobber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5.DynamicProgramming
+{
+    internal class HouseRobber
+    {
+        public int Rob(int[] amounts)
+        {
+
+        }
+
+        private int RobDynammic(int[] amounts, int position)
+        {
+            if (position == amounts.Length - 3)
+                return amounts[amounts.Length - 1];
+
+            if (position == amounts.Length - 2)
+                return 0;
+
+            var firstChoice = amounts[position] + RobDynammic(amounts, position + 2);
+            var secondChoice = amounts[position + 1] + RobDynammic(amounts, position + 3);
+
+            return Math.Max(firstChoice, secondChoice);
+        }
+    }
+}
--- ConsoleApp5/Trees/ValidateBinarySearchTree.cs
+++ ConsoleApp5/Trees/ValidateBinarySearchTree.cs
@@ -2,31 +2,32 @@
 {
     internal class ValidateBinarySearchTree
     {
-        public bool IsBstValid(TreeNode root)
+        public bool IsTreeValid(TreeNode root)
+        {
+            return IsValid(root, null, null);
+        }
+
+        /// <summary>
+        /// Every time we move to the right, we need to set minimum value as root value
+        /// Every time we move to the left, we need to set maximum value as root value
+        /// That's because current value should never go below it's root if we go right and above it's root if we go left
+        /// </summary>
+        /// <param name="root">Root node of the tree or subtree</param>
+        /// <param name="min">Node of the min value current node should not exceed</param>
+        /// <param name="max">Node of the max value current node should not exceed</param>
+        /// <returns>A boolean value indicating whether binary search tree is valid</returns>
+        private bool IsValid(TreeNode root, TreeNode min, TreeNode max)
         {
             if (root == null)
                 return true;
 
-            if (root.Left != null && root.Left.Val < root.Val)
-                return true;
+            if (min != null && min.Val > root.Val)
+                return false;
 
-            if (root.Right != null && root.Right.Val > root.Val)
-                return true;
+            if (max != null && max.Val < root.Val)
+                return false;
 
-            return IsBstValid(root.Left) && IsBstValid(root.Right);
-        }
-
-        public class TreeNode
-        {
-            public int Val;
-            public TreeNode Left;
-            public TreeNode Right;
-            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-            {
-                this.Val = val;
-                this.Left = left;
-                this.Right = right;
-            }
+            return IsValid(root.Left, min, root) && IsValid(root.Right, root, max);
         }
     }
 }
